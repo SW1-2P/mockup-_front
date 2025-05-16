5ff6e5d9a9f7ed43214ac622e665bdb324f8cc99
Shirley <gutierrez.shirley@ficct.uagrm.edu.bo>
2025-05-15 23:04:13 -0400

    first commit

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { DiagramsPage } from './DiagramsPage';
+export { NewDiagramPage } from './NewDiagramPage';
+export { TodoPage } from './TodoPage'; 
+export { NewMockupPage } from './NewMockupPage'; 
